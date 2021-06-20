25850db511faa4fa04f168dfca8ac381686df3ca
Maheswaran <maheswaran.c@cognizant.com>
2021-06-20 19:31:27 +0530

    Initial Project Added

---
+++ iOS/PlanetSample/PlanetSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlanetSample
+//
+//  Created by C, Maheswaran (Cognizant) on 20/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
